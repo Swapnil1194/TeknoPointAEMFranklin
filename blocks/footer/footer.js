d61dddccfce96cd7e935e4e85df3917b0de4e001
Bhavnesh1996 <147136450+Bhavnesh1996@users.noreply.github.com>
2023-10-06 16:41:16 +0530

    blogheader added on footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -23,3 +23,7 @@ export default async function decorate(block) {
     block.append(footer);
   }
 }
+
+
+var blogHeaderSection = document.querySelector('.table-of-contents-what-is-nps-types-of-nps-investment-plans-why-invest-in-nps-top-16-reasons-final-thoughts-frequently-asked-questions-container .default-content-wrapper p');
+blogHeaderSection.classList.add('blogs-main-heading');
